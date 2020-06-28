4af440e55acb418dc01b0bbf49ea2520a8d400cd
Jonathan Guichard <jona.guich69@gmail.com>
2020-06-28 03:54:18 +0200

    B: Change deploy processes

---
+++ deploy/production.ps1
@@ -0,0 +1,52 @@
+### Conf ###
+$hostname = '10.0.50.20'
+$username = 'ubuntu'
+$appLocation = "/var/apps/light-manager"
+$sshConnectionString = "$( $username )@$( $hostname )"
+### Conf ###
+
+### Set configuration files ###
+Write-Host "`n`n Setting up configs"
+invoke-expression -Command  "$( $PSScriptRoot )/config.ps1  prod"
+### Set configuration files ###
+
+### Build ###
+$origin = Get-Location;
+Set-Location $PSScriptRoot
+Write-Host "`nBuilding:"
+
+Write-Host "`tmanager" -NoNewline
+Set-Location ../manager
+./node_modules/.bin/tsc
+Write-Host "`tDone"
+
+Write-Host "`tfront"  -NoNewline
+Set-Location ../front
+Remove-Item -Recurse -Force  build
+yarn run build | Out-Null
+Write-Host "`tDone"
+### Build ###
+
+### Deployment ###
+Set-Location ..
+
+Write-Host "`n`nDeployment to $( $appLocation )/light-manager"
+Write-Host "`tCreating folder 'front' and 'manager'" -NoNewline
+ssh "$sshConnectionString" "mkdir -p $( $appLocation )/front && mkdir -p $( $appLocation )/manager"
+Write-Host "`tDone"
+
+
+Write-Host "`nUploading front's files"  -NoNewline
+bash.exe -c "rsync -av -e ssh  front/build $( $sshConnectionString ):$( $appLocation )/front"
+Write-Host "`tDone"
+
+Write-Host "`nUploading manager's files"  -NoNewline
+bash.exe -c  "rsync -av -e ssh  manager/build manager/package.json $( $sshConnectionString ):$( $appLocation )/manager"
+Write-Host "`tDone"
+
+Write-Host "`n`nInstalling modules in $( $appLocation )/manager" -NoNewline
+ssh "$sshConnectionString" "cd $( $appLocation )/manager && npm i"
+Write-Host "`n`nOk Deployment finished"
+### Deployment ###
+
+Set-Location $origin
